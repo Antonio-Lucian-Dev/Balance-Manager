c883f1bf2e2ad374d11436308ce52d1157cf79ce
Antonio Lucian <web.antonio.popa@gmail.com>
2021-09-12 19:16:47 +0300

    Added action btn for home page

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,35 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { ChartOptions, ChartType } from 'chart.js';
 import { Color, Label, MultiDataSet } from 'ng2-charts';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  // Doughnut
+  public doughnutChartLabels: Label[] = ['Withdrawals', 'Balance'];
+  public doughnutChartData: MultiDataSet = [[450, 1000]];
+  public doughnutChartType: ChartType = 'doughnut';
+  public color: Color[] = [{ backgroundColor: ['#E94560', '#150050'] }];
 
- // Doughnut
- public doughnutChartLabels: Label[] = ['Withdrawals', 'Balance'];
- public doughnutChartData: MultiDataSet = [
-   [450, 1000],
- ];
- public doughnutChartType: ChartType = 'doughnut';
- public color: Color[] = [
-   {backgroundColor: ['#E94560', '#150050']}
- ];
-
- public option: ChartOptions = {
-  legend: {
+  public option: ChartOptions = {
+    legend: {
       labels: {
-          fontColor: 'white',
-          fontSize: 18
-      }
-  }
-};
+        fontColor: 'white',
+        fontSize: 18,
+      },
+    },
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
